f20746e8a2e2531844d674981e2a6d5fff529c53
ishika <ishika20_ug@cse.nits.ac.in>
2021-09-21 19:15:55 +0530

    reverted

--- DB_connection/src/db/dbConnection.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/aashroy", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("connection done")) //sort arrow function is udes here
-    .catch((err) => console.log("error ho gya h"));
-
